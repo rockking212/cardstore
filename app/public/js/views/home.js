e5f1df6c1199f8b46a523d06db1e808fa0dca4f9
rockking212 <rockking212@hotmail.com>
2013-01-30 17:21:02 +0800

    Update app/public/js/views/home.js
    translate.

--- app/public/js/views/home.js
+++ app/public/js/views/home.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 
 	var hc = new HomeController();
@@ -30,8 +29,8 @@ $(document).ready(function(){
 
 // customize the account settings form //
 	
-	$('#account-form h1').text('Account Settings');
-	$('#account-form #sub1').text('Here are the current settings for your account.');
+	$('#account-form h1').text('账户设置');
+	$('#account-form #sub1').text('这里是您现在的设置.');
 	$('#user-tf').attr('disabled', 'disabled');
 	$('#account-form-btn1').html('Delete');
 	$('#account-form-btn1').addClass('btn-danger');
@@ -40,8 +39,8 @@ $(document).ready(function(){
 // setup the confirm window that displays when the user chooses to delete their account //
 
 	$('.modal-confirm').modal({ show : false, keyboard : true, backdrop : true });
-	$('.modal-confirm .modal-header h3').text('Delete Account');
-	$('.modal-confirm .modal-body p').html('Are you sure you want to delete your account?');
+	$('.modal-confirm .modal-header h3').text('删除账户');
+	$('.modal-confirm .modal-body p').html('您确认要删除这里的一切么?');
 	$('.modal-confirm .cancel').html('Cancel');
 	$('.modal-confirm .submit').html('Delete');
 	$('.modal-confirm .submit').addClass('btn-danger');
